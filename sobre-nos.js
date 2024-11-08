d312e1f5f061003cecc9ca4e9812a357945111d9
KSgui <guilhermekoboldt@gmail.com>
2024-11-07 23:05:09 -0300

    update2.0

--- sobre-nos.js
+++ sobre-nos.js
@@ -26,3 +26,28 @@ window.addEventListener('load', () => {
 //     container.querySelector('.imagem2').style.clipPath = `inset(0 ${rect.width - xPos}px 0 0)`;
 //     setas.style.left = `${xPos}px`;
 // });
+
+let count = 1;
+let intervalId = setInterval(nextImage, 5000);
+
+document.getElementById("radio1").checked = true;
+
+document.addEventListener("contextmenu", function (event) {
+    event.preventDefault();
+    clearInterval(intervalId);
+});
+
+document.addEventListener("mouseup", function (event) {
+    if (event.button === 2) {
+        intervalId = setInterval(nextImage, 4000);
+    }
+});
+
+function nextImage() {
+    count++;
+    if (count > 4) {
+        count = 1;
+    }
+
+    document.getElementById("radio" + count).checked = true;
+}
